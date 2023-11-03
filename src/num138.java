09da45664899d84cdb6489d3801da24fb3439320
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2023-11-03 09:34:10 +0800

    update

--- src/num138.java
+++ src/num138.java
@@ -5,11 +5,19 @@ class Node {
     int val;
     Node next;
     Node random;
+    public Node left;
+    public Node right;
 
     public Node(int val) {
         this.val = val;
         this.next = null;
         this.random = null;
+    }
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
     }
 }
 public class num138 {
