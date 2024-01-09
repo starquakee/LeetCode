a5353297f2ea6ae7381b2b0e2c5ede07dc926906
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2024-01-09 14:45:33 +0800

    update

---
+++ src/num2707.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class num2707 {
+    public int minExtraChar(String s, String[] dictionary) {
+        int n = s.length();
+        int[] d = new int[n + 1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String str : dictionary) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        d[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            d[i] = d[i - 1] + 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (map.containsKey(s.substring(j, i))) {
+                    d[i] = Math.min(d[i], d[j]);
+                }
+            }
+        }
+        return d[n];
+    }
+}
