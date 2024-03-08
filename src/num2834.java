df04fd8e5228a75b8f5e5c9e9418ea87e9d738d0
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2024-03-08 11:22:36 +0800

    update

---
+++ src/num2834.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class num2834 {
+    public int minimumPossibleSum(int n, int target) {
+        final int MOD = (int) 1e9 + 7;
+        int m = target / 2;
+        if (n <= m) {
+            return (int) ((long) (1 + n) * n / 2 % MOD);
+        }
+        return (int) (((long) (1 + m) * m / 2 +
+                ((long) target + target + (n - m) - 1) * (n - m) / 2) % MOD);
+    }
+}
