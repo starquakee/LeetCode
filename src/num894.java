50f02ef915f44a9d81a744630a9ad3bd95fee0f8
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2024-04-02 14:44:40 +0800

    update

---
+++ src/num894.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class num894 {
+    public List<TreeNode> allPossibleFBT(int n) {
+        List<TreeNode> ans = new ArrayList<>();
+        if(n%2==0)return ans;
+        if(n==1){
+            ans.add(new TreeNode(0));
+            return ans;
+        }
+        for(int i=1;i<n;i+=2){
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(n-i-1);
+            for(TreeNode l:left){
+                for(TreeNode r:right){
+                    TreeNode root = new TreeNode(0);
+                    root.left = l;
+                    root.right = r;
+                    ans.add(root);
+                }
+            }
+        }
+        return ans;
+    }
+}
