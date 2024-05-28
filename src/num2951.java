4c88b1d99d345615b35bb17d7a54b354111fa174
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2024-05-28 17:23:18 +0800

    update

---
+++ src/num2951.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class num2951 {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i=1;i<mountain.length-1;i++){
+            if(mountain[i]>mountain[i-1]&&mountain[i]>mountain[i+1]){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
