56f7e270452776b7c3861c353aa4323acadd93c0
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2024-12-15 13:11:19 +0800

    update

---
+++ src/num1338.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class num1338 {
+    public int minSetSize(int[] arr) {
+        if(arr.length==1)return 1;
+        Arrays.sort(arr);
+       List<Integer> list = new ArrayList<>();
+        int count = 1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==arr[i-1]){
+                count++;
+            }else {
+                list.add(count);
+                count=1;
+            }
+        }
+        list.add(count);
+        Collections.sort(list);
+        int ans = 0;
+        int num = 0;
+        while (num<arr.length/2){
+            num += list.remove(list.size()-1);
+            ans++;
+        }
+        return ans;
+    }
+}
