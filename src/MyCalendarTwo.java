4a52124ab71d6fbe0e97448bc9e8ef124428c324
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-01-03 14:20:20 +0800

    update

---
+++ src/MyCalendarTwo.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class MyCalendarTwo {
+    List<int[]> booked;
+    List<int[]> overlaps;
+
+    public MyCalendarTwo() {
+        booked = new ArrayList<int[]>();
+        overlaps = new ArrayList<int[]>();
+    }
+
+    public boolean book(int start, int end) {
+        for (int[] arr : overlaps) {
+            int l = arr[0], r = arr[1];
+            if (l < end && start < r) {
+                return false;
+            }
+        }
+        for (int[] arr : booked) {
+            int l = arr[0], r = arr[1];
+            if (l < end && start < r) {
+                overlaps.add(new int[]{Math.max(l, start), Math.min(r, end)});
+            }
+        }
+        booked.add(new int[]{start, end});
+        return true;
+    }
+}
