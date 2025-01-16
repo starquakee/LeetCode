8ad34e66d9f0a1f4fcb88c0f26d6b6212a31c354
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-01-16 16:56:32 +0800

    update

--- src/num3095.java
+++ src/num3095.java
@@ -1,14 +1,32 @@
-import java.util.Arrays;
-import java.util.Collections;
-
 public class num3095 {
+    // sliding window
     public int minimumSubarrayLength(int[] nums, int k) {
-        Arrays.sort(nums);
-        int temp=0;
-        for(int i=nums.length-1;i>=0;i--){
-            temp = temp|nums[i];
-            if(temp>=k)return nums.length-i;
+        int left=0;
+        int right=0;
+        int[] bits = new int[30];
+        int ans = Integer.MAX_VALUE;
+        while (right<nums.length){
+            for(int i=0;i<30;i++){
+                bits[i] += (1<<i)&nums[right];
+            }
+            int num = bits2num(bits);
+            while (num>=k&&left<nums.length&&right>=left){
+                ans = Math.min(ans, right-left+1);
+                for(int i=0;i<30;i++){
+                    bits[i] -= (1<<i)&nums[left];
+                }
+                left++;
+                num = bits2num(bits);
+            }
+            right++;
         }
-        return -1;
+        return ans==Integer.MAX_VALUE?-1:ans;
+    }
+    public int bits2num(int[] bits){
+        int num=0;
+        for(int i=0;i<bits.length;i++){
+            if(bits[i]>0)num+= (int) Math.pow(2,i);
+        }
+        return num;
     }
 }
