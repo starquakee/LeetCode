92bce54ec211c0715b483aa2df89496e2663fde7
starquakee <752280856@qq.com>
2025-01-27 21:43:01 +0800

    update

--- src/num45.java
+++ src/num45.java
@@ -1,21 +1,52 @@
+import java.util.Arrays;
+
 public class num45 {
+//    public int jump(int[] nums) {
+//        int n = nums.length;
+//        int ans = 0;
+//        int place = 0;
+//        while (place<n-1){
+//            int max_len = 0;
+//            int next_place = 0;
+//            for(int j=1;j<=nums[place];j++){
+//                if(place+j>=n-1) return ans+1;
+//                if(nums[place+j]+place+j>max_len){
+//                    max_len = nums[place+j]+place+j;
+//                    next_place = place+j;
+//                }
+//            }
+//            ans++;
+//            place = next_place;
+//        }
+//        return ans;
+//    }
+//    public int jump(int[] nums) {
+//        int n = nums.length;
+//        int[] ans = new int[n];
+//        Arrays.fill(ans,Integer.MAX_VALUE);
+//        ans[0]=0;
+//        for(int i=0;i<n;i++){
+//            for(int j=1;j<=nums[i]&&i+j<n;j++){
+//                ans[i+j]=Math.min(ans[i+j],ans[i]+1);
+//            }
+//        }
+//        return ans[n-1];
+//    }
+    // ans is monotonically increasing
     public int jump(int[] nums) {
         int n = nums.length;
-        int ans = 0;
-        int place = 0;
-        while (place<n-1){
-            int max_len = 0;
-            int next_place = 0;
-            for(int j=1;j<=nums[place];j++){
-                if(place+j>=n-1) return ans+1;
-                if(nums[place+j]+place+j>max_len){
-                    max_len = nums[place+j]+place+j;
-                    next_place = place+j;
-                }
+        int[] ans = new int[n];
+        int updating = 0;
+        for(int i=0;i<n;i++){
+            int j=updating+1-i;
+            for(;j<=nums[i]&&i+j<n;j++){
+                ans[i+j]=ans[i]+1;
             }
-            ans++;
-            place = next_place;
+            updating=i+j-1;
+            System.out.println(updating);
+            if (updating==n-1)break;
         }
-        return ans;
+        System.out.println(Arrays.toString(ans));
+        return ans[n-1];
     }
 }
