f087a0456773537f341785e7b072a280adade7e0
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-03-15 09:30:52 +0800

    update

---
+++ src/num3110.java
@@ -0,0 +1,9 @@
+public class num3110 {
+    public int scoreOfString(String s) {
+        int ans=0;
+        for(int i=1;i<s.length();i++){
+            ans+=Math.abs(s.charAt(i)-s.charAt(i-1));
+        }
+        return ans;
+    }
+}
