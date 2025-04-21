4e2a03ea86f4a3428686abaeecf577ba75bde252
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-04-21 17:15:19 +0800

    update

--- src/ThreadPoolExample.java
+++ src/ThreadPoolExample.java
@@ -12,5 +12,22 @@ public class ThreadPoolExample {
         }
 
         executor.shutdown(); // 关闭线程池（不再接收新任务）
+
+        ExecutorService pool = new ThreadPoolExecutor(
+                2,               // corePoolSize
+                5,               // maximumPoolSize
+                60,              // keepAliveTime
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(10), // 阻塞队列
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy() // 拒绝策略
+        );
+        for (int i = 1; i <= 5; i++) {
+            int taskId = i;
+            pool.execute(() -> {
+                System.out.println("执行任务：" + taskId + "，线程：" + Thread.currentThread().getName());
+            });
+        }
+
     }
 }
