77ea6a6dbd4251dea3457a39c0b754af5a2511d0
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-05-01 13:38:45 +0800

    update

---
+++ src/num2071.java
@@ -0,0 +1,48 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.TreeMap;
+
+public class num2071 {
+    public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
+        int n = tasks.length, m = workers.length;
+        Arrays.sort(tasks);
+        Arrays.sort(workers);
+        int left = 1, right = Math.min(m, n), ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (check(tasks, workers, pills, strength, mid)) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean check(int[] tasks, int[] workers, int pills, int strength, int mid) {
+        int p = pills;
+        int m = workers.length;
+        Deque<Integer> ws = new ArrayDeque<>();
+        int ptr = m - 1;
+        for (int i = mid - 1; i >= 0; --i) {
+            while (ptr >= m - mid && workers[ptr] + strength >= tasks[i]) {
+                ws.addFirst(workers[ptr]);
+                --ptr;
+            }
+            if (ws.isEmpty()) {
+                return false;
+            } else if (ws.getLast() >= tasks[i]) {
+                ws.pollLast();
+            } else {
+                if (p == 0) {
+                    return false;
+                }
+                --p;
+                ws.pollFirst();
+            }
+        }
+        return true;
+    }
+}
