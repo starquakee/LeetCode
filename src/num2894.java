ba3ac6d55ce1bf916474fd9d3dc34d7c8d1dd019
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-05-27 00:26:19 +0800

    update

---
+++ src/num2894.java
@@ -0,0 +1,10 @@
+public class num2894 {
+    public int differenceOfSums(int n, int m) {
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(i%m==0)ans-=i;
+            else ans+=i;
+        }
+        return ans;
+    }
+}
