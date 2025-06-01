0f241133ca024bec96b42ebacbf8d5a180c32a89
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-06-01 10:42:24 +0800

    update

---
+++ src/num2929.java
@@ -0,0 +1,12 @@
+public class num2929 {
+    public long distributeCandies(int n, int limit) {
+        long ans = 0L;
+        for (int i = 0; i <= Math.min(limit, n); i++) {
+            if (n - i > 2 * limit) {
+                continue;
+            }
+            ans += Math.min(n - i, limit) - Math.max(0, n - i - limit) + 1;
+        }
+        return ans;
+    }
+}
