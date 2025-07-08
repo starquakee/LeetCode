2389dd03d615537cd46b70621187f9172fe3facc
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-07-08 17:48:29 +0800

    update

--- src/num518.java
+++ src/num518.java
@@ -1,13 +1,10 @@
 public class num518 {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
         int[] dp = new int[amount+1];
         dp[0] = 1;
         for(int coin:coins){
             for(int i=coin;i<=amount;i++){
-                if(coin<=i){
-                    dp[i] += dp[i-coin];
-                }
+                dp[i] += dp[i-coin];
             }
         }
         return dp[amount];
