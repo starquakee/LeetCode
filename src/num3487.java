d297311b3e2dfd5148de6698288832b572529b1e
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-07-25 14:49:13 +0800

    update

--- src/num3487.java
+++ src/num3487.java
@@ -1,17 +1,17 @@
+import java.util.Arrays;
+
 public class num3487 {
     public int maxSum(int[] nums) {
         boolean[] visited = new boolean[101];
         int ans=0;
-        int max=-1000;
         for(int num:nums){
-            max=Math.max(max,num);
             if(num>0&&!visited[num]){
                 visited[num]=true;
                 ans+=num;
             }
         }
         if(ans==0){
-            return max;
+            return Arrays.stream(nums).max().getAsInt();
         }
         return ans;
     }
