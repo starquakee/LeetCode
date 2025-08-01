8d9bdb11fc3f4077b19afcffad18326382f55600
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-08-01 14:45:17 +0800

    update

--- src/num118.java
+++ src/num118.java
@@ -2,21 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class num118 {
+//    public List<List<Integer>> generate(int numRows) {
+//        List<List<Integer>> ans = new ArrayList<>();
+//        int[][] arr = new int[numRows][numRows];
+//        for(int i=0;i<numRows;i++){
+//            List<Integer> temp = new ArrayList<>();
+//            for(int j=0;j<=i;j++){
+//                if(j==0||j==i){
+//                    arr[i][j] = 1;
+//                }else {
+//                    arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+//                }
+//                temp.add(arr[i][j]);
+//            }
+//            ans.add(temp);
+//        }
+//        return ans;
+//    }
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int[][] arr = new int[numRows][numRows];
-        for(int i=0;i<numRows;i++){
-            List<Integer> temp = new ArrayList<>();
-            for(int j=0;j<=i;j++){
-                if(j==0||j==i){
-                    arr[i][j] = 1;
-                }else {
-                    arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
                 }
-                temp.add(arr[i][j]);
             }
-            ans.add(temp);
+            ret.add(row);
         }
-        return ans;
+        return ret;
     }
 }
