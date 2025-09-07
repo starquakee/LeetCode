a5b385340d42e57d2d46a159fb76cc6fa6fbfbd9
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-09-07 22:23:45 +0800

    update

--- src/num70.java
+++ src/num70.java
@@ -10,14 +10,24 @@ public class num70 {
 //        }
 //        return dp[n];
 //    }
+//    public int climbStairs(int n) {
+//        if(n==1)return 1;
+//        if(n==2)return 2;
+//        int[] dp = new int[n+1];
+//        dp[1] = 1;
+//        dp[2] = 2;
+//        for(int i=3;i<=n;i++){
+//            dp[i] = dp[i-2]+dp[i-1];
+//        }
+//        return dp[n];
+//    }
     public int climbStairs(int n) {
-        if(n==1)return 1;
-        if(n==2)return 2;
-        int[] dp = new int[n+1];
+        int num=Math.max(n+1,3);
+        int[] dp = new int[num];
         dp[1] = 1;
         dp[2] = 2;
         for(int i=3;i<=n;i++){
-            dp[i] = dp[i-2]+dp[i-1];
+            dp[i] = dp[i-1]+dp[i-2];
         }
         return dp[n];
     }
