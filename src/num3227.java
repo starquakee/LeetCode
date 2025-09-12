447f193a6d390be6e44575caa8a6681afed698ed
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-09-12 09:27:21 +0800

    update

---
+++ src/num3227.java
@@ -0,0 +1,5 @@
+public class num3227 {
+    public boolean doesAliceWin(String s) {
+        return s.contains("a") || s.contains("e") || s.contains("i") || s.contains("o") || s.contains("u");
+    }
+}
