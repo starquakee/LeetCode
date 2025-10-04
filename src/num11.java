7766669b1db8eecb151956d8bcc4d5e52d54be2d
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-04 11:07:39 +0800

    update

--- src/num11.java
+++ src/num11.java
@@ -1,25 +1,37 @@
 public class num11 {
-    public  static int maxArea(int[] height) {
-        int max = 0;
-        int left_max = 0;
-        for(int i=0;i<height.length;i++){
-            if (height[i]>left_max){
-                left_max = height[i];
-                int max_height = 0;
+//    public  static int maxArea(int[] height) {
+//        int max = 0;
+//        int left_max = 0;
+//        for(int i=0;i<height.length;i++){
+//            if (height[i]>left_max){
+//                left_max = height[i];
+//                int max_height = 0;
+//                for(int j=height.length-1;j>i;j--){
+//                    if(height[j]>max_height){
+//                        max_height = height[j];
+//                        max = Math.max(max, (j-i) * Math.min(height[i], max_height));
+//                    }
+//                }
+//            }
+//
+//        }
+//        return max;
+//    }
+    public int maxArea(int[] height) {
+        int ans=0;
+        int leftMax=0;
+        for(int i=0;i<height.length-1;i++){
+            if(height[i]>leftMax){
+                leftMax=height[i];
+                int rightMax=0;
                 for(int j=height.length-1;j>i;j--){
-                    if(height[j]>max_height){
-                        max_height = height[j];
-                        max = Math.max(max, (j-i) * Math.min(height[i], max_height));
+                    if(height[j]>rightMax){
+                        rightMax=height[j];
+                        ans=Math.max(ans,Math.min(leftMax,rightMax)*(j-i));
                     }
                 }
             }
-
         }
-        return max;
-    }
-    public static void main(String[] args) {
-        int[] a = new int[]{1,8,6,2,5,4,8,3,7};
-        System.out.println(maxArea(a));
-
+        return ans;
     }
 }
