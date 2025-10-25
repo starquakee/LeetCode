59785d6b69cfcf7fbc47b2b2f8437a4ce919d10b
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-25 21:50:48 +0800

    update

---
+++ src/lcr24.java
@@ -0,0 +1,13 @@
+public class lcr24 {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
