9da24aaa061f69117677d9e61b502e7f838e7ca1
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-25 17:45:03 +0800

    update

--- src/num19.java
+++ src/num19.java
@@ -1,22 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class num19 {
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
+//        int length = getListNodeLength(head);
+//        ListNode begin = new ListNode(-1);
+//        begin.next = head;
+//        ListNode current = begin;
+//        for(int i=0;i<length-n;i++){
+//            current=current.next;
+//        }
+//        current.next = current.next.next;
+//        return begin.next;
+//    }
+//    public int getListNodeLength(ListNode listNode){
+//        int ans = 0;
+//        while (listNode != null){
+//            listNode = listNode.next;
+//            ans++;
+//        }
+//        return ans;
+//    }
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int length = getListNodeLength(head);
-        ListNode begin = new ListNode(-1);
-        begin.next = head;
-        ListNode current = begin;
-        for(int i=0;i<length-n;i++){
-            current=current.next;
+        List<ListNode> list = new ArrayList<>();
+        ListNode curr = head;
+        while (curr != null) {
+            list.add(curr);
+            curr = curr.next;
         }
-        current.next = current.next.next;
-        return begin.next;
-    }
-    public int getListNodeLength(ListNode listNode){
-        int ans = 0;
-        while (listNode != null){
-            listNode = listNode.next;
-            ans++;
-        }
-        return ans;
+        if(list.size()<n+1){return head.next;}
+        list.get(list.size() - n - 1).next = list.get(list.size() - n).next;
+        return head;
     }
 
     public static void main(String[] args) {
