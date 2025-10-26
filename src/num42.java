0433cf69c7a1db0697a788b00389746c6d6d2cba
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-26 20:55:23 +0800

    update

--- src/num42.java
+++ src/num42.java
@@ -56,27 +56,41 @@ public class num42 {
 //        }
 //        return ans;
 //    }
+//    public int trap(int[] height) {
+//        int n = height.length;
+//        if (n == 0) {
+//            return 0;
+//        }
+//
+//        int[] leftMax = new int[n];
+//        leftMax[0] = height[0];
+//        for (int i = 1; i < n; ++i) {
+//            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+//        }
+//
+//        int[] rightMax = new int[n];
+//        rightMax[n - 1] = height[n - 1];
+//        for (int i = n - 2; i >= 0; --i) {
+//            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+//        }
+//
+//        int ans = 0;
+//        for (int i = 0; i < n; ++i) {
+//            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+//        }
+//        return ans;
+//    }
     public int trap(int[] height) {
-        int n = height.length;
-        if (n == 0) {
-            return 0;
+        int n=height.length;
+        int[] leftMax=new int[n];
+        int[] rightMax=new int[n];
+        for(int i=1;i<n;i++){
+            leftMax[i]=Math.max(leftMax[i-1],height[i-1]);
+            rightMax[n-i-1]=Math.max(rightMax[n-i],height[n-i]);
         }
-
-        int[] leftMax = new int[n];
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; ++i) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
-        }
-
-        int[] rightMax = new int[n];
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; --i) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
-        }
-
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        int ans=0;
+        for(int i=1;i<n-1;i++){
+            ans+=Math.max(Math.min(leftMax[i],rightMax[i])-height[i],0);
         }
         return ans;
     }
