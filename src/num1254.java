852c4d4e5594df7b35655e9e101004005c1d7008
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-27 16:57:52 +0800

    update

--- src/num1254.java
+++ src/num1254.java
@@ -2,45 +2,73 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class num1254 {
-    public static int closedIsland(int[][] grid) {
-        int ans = 0;
-        int[][] direction = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
-        int m = grid.length;
-        int n = grid[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-//                System.out.println(i+" "+j);
+//    public static int closedIsland(int[][] grid) {
+//        int ans = 0;
+//        int[][] direction = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+//        int m = grid.length;
+//        int n = grid[0].length;
+//        for(int i=0;i<m;i++){
+//            for(int j=0;j<n;j++){
+//                if(grid[i][j]==0){
+//                    Queue<int[]> queue = new ArrayDeque<>();
+//                    queue.offer(new int[]{i,j});
+//                    grid[i][j]=1;
+//                    boolean closed = true;
+//                    while (!queue.isEmpty()){
+//                        int[] arr = queue.poll();
+//                        int x = arr[0];
+//                        int y = arr[1];
+//                        if(x==0||x==m-1||y==0||y==n-1){
+//                            closed = false;
+//                        }
+//                        for(int count=0;count<4;count++){
+//                            int x_new = x+direction[count][0];
+//                            int y_new = y+direction[count][1];
+//                            if(x_new>=0&&x_new<m&&y_new>=0&&y_new<n&&grid[x_new][y_new]==0){
+//                                grid[x_new][y_new] = 1;
+//                                queue.offer(new int[]{x_new,y_new});
+//                            }
+//                        }
+//                    }
+//                    if(closed){
+//                        ans++;
+//                    }
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+    public int closedIsland(int[][] grid) {
+        int ans=0;
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][] dirs=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+        Queue<int[]> queue = new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==0){
-                    Queue<int[]> queue = new ArrayDeque<>();
                     queue.offer(new int[]{i,j});
                     grid[i][j]=1;
-                    boolean closed = true;
+                    boolean flag=true;
                     while (!queue.isEmpty()){
-                        int[] arr = queue.poll();
-                        int x = arr[0];
-                        int y = arr[1];
-                        if(x==0||x==m-1||y==0||y==n-1){
-                            closed = false;
-                        }
-                        for(int count=0;count<4;count++){
-                            int x_new = x+direction[count][0];
-                            int y_new = y+direction[count][1];
-                            if(x_new>=0&&x_new<m&&y_new>=0&&y_new<n&&grid[x_new][y_new]==0){
-                                grid[x_new][y_new] = 1;
-                                queue.offer(new int[]{x_new,y_new});
+                        int[] poll = queue.poll();
+                        int ii=poll[0];
+                        int jj=poll[1];
+                        if(ii==0||ii==n-1||jj==0||jj==m-1) flag=false;
+                        for(int k=0;k<dirs.length;k++){
+                            int newX=ii+dirs[k][0];
+                            int newY=jj+dirs[k][1];
+                            if(newX>=0&&newX<n&&newY>=0&&newY<m&&grid[newX][newY]==0){
+                                queue.offer(new int[]{newX,newY});
+                                grid[newX][newY]=1;
                             }
                         }
                     }
-                    if(closed){
-                        ans++;
-                    }
+                    if(flag) ans++;
                 }
             }
         }
         return ans;
     }
 
-    public static void main(String[] args) {
-        System.out.println(closedIsland(new int[][]{{1,1,1,1,1,1,1,0},{1,0,0,0,0,1,1,0},{1,0,1,0,1,1,1,0},{1,0,0,0,0,1,0,1},{1,1,1,1,1,1,1,0}}));
-    }
 }
