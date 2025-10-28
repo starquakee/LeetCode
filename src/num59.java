2ae15424fde24f1634353b5b412532f8a6bd90b9
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-28 17:25:18 +0800

    update

--- src/num59.java
+++ src/num59.java
@@ -1,28 +1,49 @@
 public class num59 {
+//    public int[][] generateMatrix(int n) {
+//        int[][] ans = new int[n][n];
+//        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+//        int num=1;
+//        int i=0,j=0;
+//        ans[0][0]=1;
+//        int d=0;
+//        for(int z=1;z<n*n;z++){
+//            int newi=i+dirs[d%4][0];
+//            int newj=j+dirs[d%4][1];
+//            if(newi<n&&newi>=0&&newj<n&&newj>=0){
+//                if (ans[newi][newj] != 0) {
+//                    d++;
+//                    newi = i + dirs[d % 4][0];
+//                    newj = j + dirs[d % 4][1];
+//                }
+//            }else {
+//                d++;
+//                newi=i+dirs[d%4][0];
+//                newj=j+dirs[d%4][1];
+//            }
+//            ans[newi][newj]=++num;
+//            i=newi;
+//            j=newj;
+//        }
+//        return ans;
+//    }
     public int[][] generateMatrix(int n) {
         int[][] ans = new int[n][n];
         int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int num=1;
-        int i=0,j=0;
-        ans[0][0]=1;
-        int d=0;
-        for(int z=1;z<n*n;z++){
-            int newi=i+dirs[d%4][0];
-            int newj=j+dirs[d%4][1];
-            if(newi<n&&newi>=0&&newj<n&&newj>=0){
-                if (ans[newi][newj] != 0) {
-                    d++;
-                    newi = i + dirs[d % 4][0];
-                    newj = j + dirs[d % 4][1];
-                }
+        int x=0;
+        int y=0;
+        int count=0;
+        for(int i=1;i<=n*n;i++){
+            ans[x][y]=i;
+            int newx=x+dirs[count%4][0];
+            int newy=y+dirs[count%4][1];
+            if(newx>=0&&newx<n&&newy>=0&&newy<n&&ans[newx][newy]==0){
+                x=newx;
+                y=newy;
             }else {
-                d++;
-                newi=i+dirs[d%4][0];
-                newj=j+dirs[d%4][1];
+                count++;
+                x+=dirs[count%4][0];
+                y+=dirs[count%4][1];
             }
-            ans[newi][newj]=++num;
-            i=newi;
-            j=newj;
         }
         return ans;
     }
