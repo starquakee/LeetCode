303c0dfe6d499584c71e99b0c60210f31a9d17f9
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-29 23:55:40 +0800

    update

--- src/num239.java
+++ src/num239.java
@@ -30,6 +30,22 @@ public class num239 {
 //        }
 //        return ans;
 //    }
+//    public int[] maxSlidingWindow(int[] nums, int k) {
+//        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> b[0]-a[0]);
+//        for(int i=0;i<k;i++){
+//            pq.offer(new int[]{nums[i],i});
+//        }
+//        int[] ans = new int[nums.length-k+1];
+//        ans[0] = pq.peek()[0];
+//        for(int i=k;i<nums.length;i++){
+//            pq.offer(new int[]{nums[i],i});
+//            while (pq.peek()[1]<=i-k){
+//                pq.poll();
+//            }
+//            ans[i-k+1] = pq.peek()[0];
+//        }
+//        return ans;
+//    }
     public int[] maxSlidingWindow(int[] nums, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> b[0]-a[0]);
         for(int i=0;i<k;i++){
@@ -39,7 +55,7 @@ public class num239 {
         ans[0] = pq.peek()[0];
         for(int i=k;i<nums.length;i++){
             pq.offer(new int[]{nums[i],i});
-            while (pq.peek()[1]<=i-k){
+            while (pq.peek()[1]+k<=i){
                 pq.poll();
             }
             ans[i-k+1] = pq.peek()[0];
