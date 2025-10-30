a92dd0108667dd51fab44a9f75287ec2ae8557a7
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-30 10:21:32 +0800

    update

---
+++ src/num1526.java
@@ -0,0 +1,9 @@
+public class num1526 {
+    public int minNumberOperations(int[] target) {
+        int ans=target[0];
+        for(int i=1;i<target.length;i++){
+            ans+=Math.max(target[i]-target[i-1],0);
+        }
+        return ans;
+    }
+}
