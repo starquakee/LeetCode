8a354c8bbc7e9a007364fc444bb2b6b220aea24f
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-30 15:13:35 +0800

    update

--- src/num560.java
+++ src/num560.java
@@ -1,13 +1,29 @@
 public class num560 {
+//    public int subarraySum(int[] nums, int k) {
+//        int count = 0;
+//        for(int i=0;i<nums.length;i++){
+//            int sum = 0;
+//            for(int j=i;j<nums.length;j++){
+//                sum += nums[j];
+//                if(sum==k) count++;
+//            }
+//        }
+//        return count;
+//    }
     public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        for(int i=0;i<nums.length;i++){
-            int sum = 0;
-            for(int j=i;j<nums.length;j++){
-                sum += nums[j];
-                if(sum==k) count++;
+        int n=nums.length;
+        int[] preSum=new int[n];
+        preSum[0]=nums[0];
+        for(int i=1;i<n;i++){
+            preSum[i]=preSum[i-1]+nums[i];
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(preSum[i]==k)ans++;
+            for(int j=0;j<i;j++){
+                if(preSum[i]-preSum[j]==k)ans++;
             }
         }
-        return count;
+        return ans;
     }
 }
