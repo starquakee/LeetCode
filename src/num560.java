7c4f35edb571048440197f0d3331c25bca22ec86
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-30 15:20:01 +0800

    update

--- src/num560.java
+++ src/num560.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class num560 {
 //    public int subarraySum(int[] nums, int k) {
 //        int count = 0;
@@ -12,17 +15,17 @@ public class num560 {
 //    }
     public int subarraySum(int[] nums, int k) {
         int n=nums.length;
+        int ans=0;
         int[] preSum=new int[n];
         preSum[0]=nums[0];
+        Map<Integer,Integer> map=new HashMap<>();
+        if(preSum[0]==k)ans++;
+        map.put(preSum[0],map.getOrDefault(preSum[0],0)+1);
         for(int i=1;i<n;i++){
             preSum[i]=preSum[i-1]+nums[i];
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
             if(preSum[i]==k)ans++;
-            for(int j=0;j<i;j++){
-                if(preSum[i]-preSum[j]==k)ans++;
-            }
+            ans+=map.getOrDefault(preSum[i]-k,0);
+            map.put(preSum[i],map.getOrDefault(preSum[i],0)+1);
         }
         return ans;
     }
