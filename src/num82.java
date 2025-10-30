263c7badc7595bd68f2fccd664a378de86e00e8b
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-10-30 21:35:29 +0800

    update

--- src/num82.java
+++ src/num82.java
@@ -1,21 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class num82 {
+//    public ListNode deleteDuplicates(ListNode head) {
+//        if(head==null)return null;
+//        ListNode pre = new ListNode(-1);
+//        ListNode ans = pre;
+//        pre.next = head;
+//        while (head!=null&&head.next!=null){
+//            if(head.val==head.next.val){
+//                while ((head.next!=null&&head.val==head.next.val)){
+//                    head = head.next;
+//                }
+//                pre.next = head.next;
+//                head = head.next;
+//            }else {
+//                pre = head;
+//                head = head.next;
+//            }
+//        }
+//        return ans.next;
+//    }
     public ListNode deleteDuplicates(ListNode head) {
-        if(head==null)return null;
-        ListNode pre = new ListNode(-1);
-        ListNode ans = pre;
-        pre.next = head;
-        while (head!=null&&head.next!=null){
-            if(head.val==head.next.val){
-                while ((head.next!=null&&head.val==head.next.val)){
-                    head = head.next;
-                }
-                pre.next = head.next;
-                head = head.next;
-            }else {
-                pre = head;
-                head = head.next;
-            }
+        Map<Integer,Integer> map = new HashMap<>();
+        ListNode curr=head;
+        while (curr!=null){
+            map.put(curr.val, map.getOrDefault(curr.val,0)+1);
+            curr=curr.next;
         }
-        return ans.next;
+        ListNode hair=new ListNode(-1,head);
+        curr=hair;
+        while (curr.next!=null){
+            ListNode next=curr.next;
+            if(map.get(next.val)>1){
+                curr.next=next.next;
+            }else curr=curr.next;
+        }
+        return hair.next;
     }
 }
