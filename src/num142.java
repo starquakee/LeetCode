f88a5bc284a9aeaafeef079a55d36357a88cb095
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-11-03 21:39:34 +0800

    update

--- src/num142.java
+++ src/num142.java
@@ -3,17 +3,6 @@ import java.util.List;
 import java.util.Set;
 
 public class num142 {
-//    public ListNode detectCycle(ListNode head) {
-//        if(head==null||head.next==null)return null;
-//        ListNode fast = head.next;
-//        ListNode slow = head;
-//        while (fast!=slow){
-//            if(fast==null||fast.next==null)return null;
-//            slow = slow.next;
-//            fast = fast.next.next;
-//        }
-//        return fast;
-//    }
     public ListNode detectCycle(ListNode head) {
         Set<ListNode> set = new HashSet<>();
         while (head!=null){
