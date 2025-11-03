69fd7cc715aa19e542ba3352915ba3e058cc2e85
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-11-03 21:58:28 +0800

    update

--- src/num21.java
+++ src/num21.java
@@ -4,19 +4,48 @@ import java.util.Collections;
 import java.util.List;
 
 public class num21 {
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        if(list1==null){
+//            return list2;
+//        }else if(list2==null){
+//            return list1;
+//        }else {
+//            if(list1.val< list2.val){
+//                list1.next = mergeTwoLists(list1.next,list2);
+//                return list1;
+//            }else {
+//                list2.next = mergeTwoLists(list1,list2.next);
+//                return list2;
+//            }
+//        }
+//    }
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null){
-            return list2;
-        }else if(list2==null){
-            return list1;
-        }else {
-            if(list1.val< list2.val){
-                list1.next = mergeTwoLists(list1.next,list2);
-                return list1;
+        ListNode hair = new ListNode(-1);
+        ListNode cur = hair;
+        while (list1!=null&&list2!=null){
+            if(list1.val<list2.val){
+                cur.next=list1;
+                cur=cur.next;
+                list1=list1.next;
             }else {
-                list2.next = mergeTwoLists(list1,list2.next);
-                return list2;
+                cur.next=list2;
+                cur=cur.next;
+                list2=list2.next;
             }
         }
+        if(list1==null){
+            while (list2!=null){
+                cur.next=list2;
+                cur=cur.next;
+                list2=list2.next;
+            }
+        }else{
+            while (list1!=null){
+                cur.next=list1;
+                cur=cur.next;
+                list1=list1.next;
+            }
+        }
+        return hair.next;
     }
 }
