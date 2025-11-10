3603393e9c0b95b9bfb2fa144785a2fa9b004246
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-11-10 11:45:08 +0800

    update

---
+++ src/num3542.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class num3542 {
+    public int minOperations(int[] nums) {
+        List<Integer> s = new ArrayList<>();
+        int res = 0;
+        for (int a : nums) {
+            while (!s.isEmpty() && s.get(s.size() - 1) > a) {
+                s.remove(s.size() - 1);
+            }
+            if (a == 0) continue;
+            if (s.isEmpty() || s.get(s.size() - 1) < a) {
+                res++;
+                s.add(a);
+            }
+        }
+        return res;
+    }
+}
