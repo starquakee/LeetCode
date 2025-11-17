7bd58896ce744836be9d25fb45d87712fa7a1a95
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-11-17 17:51:16 +0800

    update

--- src/num15.java
+++ src/num15.java
@@ -54,29 +54,53 @@ public class num15 {
 //        }
 //        return ans;
 //    }
+//    public List<List<Integer>> threeSum(int[] nums) {
+//        Arrays.sort(nums);
+//        int n=nums.length;
+//        List<List<Integer>> ans = new ArrayList<>();
+//        for(int i1=0;i1<n-2;i1++){
+//            if(i1>0&&nums[i1]==nums[i1-1]) continue;
+//            int i2=i1+1,i3=n-1;
+//            while (i2<i3){
+//                if(i2>i1+1&&nums[i2]==nums[i2-1]){
+//                    i2++;
+//                    continue;
+//                }
+//                if(nums[i1]+nums[i2]+nums[i3]<0){
+//                    i2++;
+//                }else if(nums[i1]+nums[i2]+nums[i3]>0){
+//                    i3--;
+//                }else {
+//                    List<Integer> temp = new ArrayList<>();
+//                    temp.add(nums[i1]);
+//                    temp.add(nums[i2]);
+//                    temp.add(nums[i3]);
+//                    ans.add(temp);
+//                    i2++;
+//                }
+//            }
+//        }
+//        return ans;
+//    }
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         int n=nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i1=0;i1<n-2;i1++){
-            if(i1>0&&nums[i1]==nums[i1-1]) continue;
-            int i2=i1+1,i3=n-1;
-            while (i2<i3){
-                if(i2>i1+1&&nums[i2]==nums[i2-1]){
-                    i2++;
-                    continue;
-                }
-                if(nums[i1]+nums[i2]+nums[i3]<0){
-                    i2++;
-                }else if(nums[i1]+nums[i2]+nums[i3]>0){
-                    i3--;
-                }else {
+        for(int i=0;i<n-2;i++){
+            while (i<n-2&&i>0&&nums[i]==nums[i-1])i++;
+            int k=n-1;
+            for(int j=i+1;j<n-1;j++){
+                while (j<n-1&&j>i+1&&nums[j]==nums[j-1])j++;
+                int target=-nums[i]-nums[j];
+                while (k>j&&nums[k]>target)k--;
+                if(k==j)break;
+                if(k>j&&nums[k]==target){
                     List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i1]);
-                    temp.add(nums[i2]);
-                    temp.add(nums[i3]);
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
                     ans.add(temp);
-                    i2++;
+                    while (k>j&&nums[k]==target)k--;
                 }
             }
         }
