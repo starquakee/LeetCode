b4d2dc96109e8d3b50271d64e74e467b69bb1874
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-12-13 00:16:49 +0800

    update

---
+++ src/num3606.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class num3606 {
+    public boolean check(String code, boolean isActive) {
+        for (char it : code.toCharArray()) {
+            if (it != '_' && !Character.isLetterOrDigit(it)) {
+                return false;
+            }
+        }
+        return isActive;
+    }
+    public List<String> validateCoupons(String[] code,
+                                        String[] businessLine,
+                                        boolean[] isActive) {
+        List<String>[] groups = new ArrayList[4];
+        for (int i = 0; i < 4; i++) {
+            groups[i] = new ArrayList<>();
+        }
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < code.length; i++) {
+            if (!code[i].isEmpty() && check(code[i], isActive[i])) {
+                if (businessLine[i].equals("electronics")) {
+                    groups[0].add(code[i]);
+                } else if (businessLine[i].equals("grocery")) {
+                    groups[1].add(code[i]);
+                } else if (businessLine[i].equals("pharmacy")) {
+                    groups[2].add(code[i]);
+                } else if (businessLine[i].equals("restaurant")) {
+                    groups[3].add(code[i]);
+                }
+            }
+        }
+        for (List<String> group : groups) {
+            Collections.sort(group);
+            ans.addAll(group);
+        }
+        return ans;
+    }
+}
