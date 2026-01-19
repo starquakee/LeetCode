ba065e69b870529d52c8c57b79545b6d24ec9b09
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-01-19 22:06:58 +0800

    update

---
+++ src/num1292.java
@@ -0,0 +1,34 @@
+public class num1292 {
+    public int maxSideLength(int[][] mat, int threshold) {
+        int m = mat.length, n = mat[0].length;
+        int[][] P = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                P[i][j] = P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1] + mat[i - 1][j - 1];
+            }
+        }
+
+        int l = 1, r = Math.min(m, n), ans = 0;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            boolean find = false;
+            for (int i = 1; i <= m - mid + 1; i++) {
+                for (int j = 1; j <= n - mid + 1; j++) {
+                    int sum = P[i + mid - 1][j + mid - 1] - P[i - 1][j + mid - 1] - P[i + mid - 1][j - 1] + P[i - 1][j - 1];
+                    if (sum <= threshold) {
+                        find = true;
+                        break;
+                    }
+                }
+                if (find) break;
+            }
+            if (find) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
