6cf6c6203baeab5226b0989ddcfa6ea33870c258
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-01-21 21:24:45 +0800

    update

---
+++ src/num3315.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class num3315 {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = nums.get(i);
+            if (x == 2) {
+                ans[i] = -1;
+            } else {
+                int t = ~x;
+                int lowbit = t & -t;
+                ans[i] = x ^ (lowbit >> 1);
+            }
+        }
+        return ans;
+    }
+}
