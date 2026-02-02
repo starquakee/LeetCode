3df0a7435f7a0d295b42df3a1a5bedef96ea33b4
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-02-02 22:06:07 +0800

    update

---
+++ src/num3013.java
@@ -0,0 +1,104 @@
+import java.util.TreeMap;
+
+public class num3013 {
+    private int k;
+    private int st1Size, st2Size;
+    // st1 保存前 k 小值，st2 保存其它值
+    private TreeMap<Integer, Integer> st1, st2;
+    // sm 表示前 k 小元素的和
+    private long sm;
+
+    public num3013(int k) {
+        this.k = k;
+        this.st1 = new TreeMap<>();
+        this.st2 = new TreeMap<>();
+        this.sm = 0;
+        this.st1Size = 0;
+        this.st2Size = 0;
+    }
+
+    private void removeOne(TreeMap<Integer, Integer> map, int key) {
+        int count = map.get(key);
+        if (count == 1) {
+            map.remove(key);
+        } else {
+            map.put(key, count - 1);
+        }
+    }
+
+    private void addOne(TreeMap<Integer, Integer> map, int key) {
+        map.put(key, map.getOrDefault(key, 0) + 1);
+    }
+
+    private void adjust() {
+        while (st1Size < k && !st2.isEmpty()) {
+            int x = st2.firstKey();
+            addOne(st1, x);
+            st1Size++;
+            sm += x;
+            removeOne(st2, x);
+            st2Size--;
+        }
+        while (st1Size > k) {
+            int x = st1.lastKey();
+            addOne(st2, x);
+            st2Size++;
+            removeOne(st1, x);
+            st1Size--;
+            sm -= x;
+        }
+    }
+
+    // 插入元素 x
+    public void add(int x) {
+        if (!st2.isEmpty() && x >= st2.firstKey()) {
+            addOne(st2, x);
+            st2Size++;
+        } else {
+            addOne(st1, x);
+            st1Size++;
+            sm += x;
+        }
+        adjust();
+    }
+
+    // 删除元素 x
+    public void erase(int x) {
+        if (st1.containsKey(x)) {
+            removeOne(st1, x);
+            st1Size--;
+            sm -= x;
+        } else if (st2.containsKey(x)) {
+            removeOne(st2, x);
+            st2Size--;
+        }
+        adjust();
+    }
+
+    // 前 k 小元素的和
+    public long sum() {
+        return sm;
+    }
+}
+
+class Solution1 {
+    public long minimumCost(int[] nums, int k, int dist) {
+        int n = nums.length;
+        num3013 cnt = new num3013(k - 2);
+        for (int i = 1; i < k - 1; i++) {
+            cnt.add(nums[i]);
+        }
+
+        long ans = cnt.sum() + nums[k - 1];
+        for (int i = k; i < n; i++) {
+            int j = i - dist - 1;
+            if (j > 0) {
+                cnt.erase(nums[j]);
+            }
+            cnt.add(nums[i - 1]);
+            ans = Math.min(ans, cnt.sum() + nums[i]);
+        }
+
+        return ans + nums[0];
+    }
+}
