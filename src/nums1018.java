0eb65ec9f2a54d8ec600c41e175881c7560ac952
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-02-02 21:59:59 +0800

    update

---
+++ src/nums1018.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class nums1018 {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> ans = new ArrayList<>();
+        int temp=0;
+        for(int i=0;i<nums.length;i++){
+            temp*=2;
+            temp+=nums[i];
+            temp%=5;
+            if(temp%5==0)ans.add(true);
+            else ans.add(false);
+        }
+        return ans;
+    }
+}
