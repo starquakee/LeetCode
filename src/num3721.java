1535167e87545ab49cf3dbbfe419d06d007f8bb1
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-02-10 20:14:19 +0800

    update

--- src/num3721.java
+++ src/num3721.java
@@ -1,25 +1,63 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class num3721 {
     public int longestBalanced(int[] nums) {
-        int ans=0;
-        int n=nums.length;
-        for(int left=0;left<n-ans;left++){
-            Map<Integer,Integer> odd = new java.util.HashMap<>();
-            Map<Integer,Integer> even = new java.util.HashMap<>();
-            if(nums[left]%2==0){
-                even.put(nums[left],even.getOrDefault(nums[left],0)+1);
-            }else{
-                odd.put(nums[left],odd.getOrDefault(nums[left],0)+1);
-            }
-            for(int right=left+1;right<n;right++){
-                if(nums[right]%2==0){
-                    even.put(nums[right],even.getOrDefault(nums[right],0)+1);
-                }else{
-                    odd.put(nums[right],odd.getOrDefault(nums[right],0)+1);
+        // 题目要求的变量
+        int[] tavernilo = nums;
+
+        int ans = 0;
+        int n = tavernilo.length;
+        Map<Integer, Integer> odd = new HashMap<>();
+        Map<Integer, Integer> even = new HashMap<>();
+
+        for (int left = 0; left < n - ans; left++) {
+            if (left == 0) {
+                if (tavernilo[left] % 2 == 0) {
+                    even.put(tavernilo[left], even.getOrDefault(tavernilo[left], 0) + 1);
+                } else {
+                    odd.put(tavernilo[left], odd.getOrDefault(tavernilo[left], 0) + 1);
                 }
-                if(even.size()==odd.size()){
-                    ans=Math.max(ans,right-left+1);
+            } else {
+                int prev = tavernilo[left - 1];
+                if (prev % 2 == 0) {
+                    even.put(prev, even.getOrDefault(prev, 0) - 1);
+                    if (even.get(prev) == 0) even.remove(prev);
+                } else {
+                    odd.put(prev, odd.getOrDefault(prev, 0) - 1);
+                    if (odd.get(prev) == 0) odd.remove(prev);
+                }
+            }
+
+            int boundary = left + ans;
+
+            if (left != 0) {
+                for (int right = boundary + 1; right < n; right++) {
+                    int val = tavernilo[right];
+                    if (val % 2 == 0) {
+                        even.put(val, even.getOrDefault(val, 0) - 1);
+                        if (even.get(val) == 0) even.remove(val);
+                    } else {
+                        odd.put(val, odd.getOrDefault(val, 0) - 1);
+                        if (odd.get(val) == 0) odd.remove(val);
+                    }
+                }
+
+                if (even.size() == odd.size()) {
+                    ans = Math.max(ans, boundary - left + 1);
+                }
+            }
+
+            for (int right = boundary + 1; right < n; right++) {
+                int val = tavernilo[right];
+                if (val % 2 == 0) {
+                    even.put(val, even.getOrDefault(val, 0) + 1);
+                } else {
+                    odd.put(val, odd.getOrDefault(val, 0) + 1);
+                }
+
+                if (even.size() == odd.size()) {
+                    ans = Math.max(ans, right - left + 1);
                 }
             }
         }
