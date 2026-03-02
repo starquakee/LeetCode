251bf61d01013da448762d1c40908705d3fda937
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-03-02 23:18:55 +0800

    update

---
+++ src/num1536.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class num1536 {
+    //先找到每行最后一个1的位置，记录在pos数组中。
+    // 然后从上到下遍历每行，找到第一个满足条件的行（pos[j] <= i），如果找到了，就把它交换到当前行，并更新答案。
+    public int minSwaps(int[][] grid) {
+        int n = grid.length;
+        int[] pos = new int[n];
+        Arrays.fill(pos, -1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (grid[i][j] == 1) {
+                    pos[i] = j;
+                    break;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int k = -1;
+            for (int j = i; j < n; ++j) {
+                if (pos[j] <= i) {
+                    ans += j - i;
+                    k = j;
+                    break;
+                }
+            }
+            if (k >= 0) {
+                for (int j = k; j > i; --j) {
+                    int temp = pos[j];
+                    pos[j] = pos[j - 1];
+                    pos[j - 1] = temp;
+                }
+            } else {
+                return -1;
+            }
+        }
+        return ans;
+    }
+}
