1842e4fc0d98a771d880142fd98409a56475db4c
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-03-04 00:16:24 +0800

    update

--- src/num1582.java
+++ src/num1582.java
@@ -1,20 +1,42 @@
 public class num1582 {
+//    public int numSpecial(int[][] mat) {
+//        int ans=0;
+//        for(int i=0;i<mat.length;i++){
+//            int sum=0;
+//            for(int j=0;j<mat[0].length;j++){
+//                sum+=mat[i][j];
+//            }
+//            if(sum==1){
+//                for(int j=0;j<mat[0].length;j++){
+//                    if(mat[i][j]==1){
+//                        int sum2=0;
+//                        for(int k=0;k<mat.length;k++){
+//                            sum2+=mat[k][j];
+//                        }
+//                        if(sum2==1)ans++;
+//                    }
+//                }
+//            }
+//        }
+//        return ans;
+//    }
     public int numSpecial(int[][] mat) {
         int ans=0;
-        for(int i=0;i<mat.length;i++){
-            int sum=0;
-            for(int j=0;j<mat[0].length;j++){
-                sum+=mat[i][j];
-            }
-            if(sum==1){
-                for(int j=0;j<mat[0].length;j++){
-                    if(mat[i][j]==1){
-                        int sum2=0;
-                        for(int k=0;k<mat.length;k++){
-                            sum2+=mat[k][j];
-                        }
-                        if(sum2==1)ans++;
+        int m=mat.length,n=mat[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1){
+                    int sum=0;
+                    for(int k=0;k<m;k++){
+                        sum+=mat[k][j];
                     }
+                    if(sum==1){
+                        sum=0;
+                        for(int k=0;k<n;k++){
+                            sum+=mat[i][k];
+                        }
+                        if(sum==1)ans++;
+                    }else break;
                 }
             }
         }
