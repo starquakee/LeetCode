9b6e4252f38536b81a1db972e251b66404537acd
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-03-14 13:55:27 +0800

    update

--- src/num1415.java
+++ src/num1415.java
@@ -4,11 +4,12 @@ import java.util.List;
 public class num1415 {
     public String getHappyString(int n, int k) {
         List<StringBuilder> list = new ArrayList<>();
-        backtrack(list, new StringBuilder(), n);
+        backtrack(list, new StringBuilder(), n,k);
         if(k>list.size()) return "";
         return list.get(k-1).toString();
     }
-    public void backtrack(List<StringBuilder> list, StringBuilder sb, int n){
+    public void backtrack(List<StringBuilder> list, StringBuilder sb, int n, int k){
+        if(list.size()>=k)return;
         if(sb.length()==n) {
             list.add(new StringBuilder(sb));
             return;
@@ -16,7 +17,7 @@ public class num1415 {
         for(char c='a';c<='c';c++){
             if(sb.length()>0&&sb.charAt(sb.length()-1)==c) continue;
             sb.append(c);
-            backtrack(list, sb, n);
+            backtrack(list, sb, n,k);
             sb.deleteCharAt(sb.length()-1);
         }
     }
