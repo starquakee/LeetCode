da02f9af2cd1f11015550e0f9d4025183092f4c1
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-03-28 10:38:32 +0800

    update

---
+++ src/num2573.java
@@ -0,0 +1,46 @@
+public class num2573 {
+    public String findTheString(int[][] lcp) {
+        int n = lcp.length;
+        char[] word = new char[n];
+        char current = 'a';
+
+        // 依次从 'a' 到 'z' 开始构造字符串
+        for (int i = 0; i < n; i++) {
+            if (word[i] == 0) {
+                if (current > 'z') {
+                    return "";
+                }
+                word[i] = current;
+                for (int j = i + 1; j < n; j++) {
+                    if (lcp[i][j] > 0) {
+                        word[j] = word[i];
+                    }
+                }
+                current++;
+            }
+        }
+
+        // 验证构造的字符串是否满足 LCP 矩阵要求
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (word[i] != word[j]) {
+                    if (lcp[i][j] != 0) {
+                        return "";
+                    }
+                } else {
+                    if (i == n - 1 || j == n - 1) {
+                        if (lcp[i][j] != 1) {
+                            return "";
+                        }
+                    } else {
+                        if (lcp[i][j] != lcp[i + 1][j + 1] + 1) {
+                            return "";
+                        }
+                    }
+                }
+            }
+        }
+
+        return new String(word);
+    }
+}
