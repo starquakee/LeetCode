424fdd1a25c14b897fce246d0006d075c0dc9fc0
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-03-30 22:48:26 +0800

    update

---
+++ src/num2840.java
@@ -0,0 +1,18 @@
+public class num2840 {
+    public boolean checkStrings(String s1, String s2) {
+        int[] even=new int[26];
+        int[] odd=new int[26];
+        for(int i=0;i<s1.length();i++){
+            if(i%2==0) even[s1.charAt(i)-'a']++;
+            else odd[s1.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s2.length();i++) {
+            if (i % 2 == 0) even[s2.charAt(i) - 'a']--;
+            else odd[s2.charAt(i) - 'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(even[i]!=0||odd[i]!=0) return false;
+        }
+        return true;
+    }
+}
