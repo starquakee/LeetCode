9344620009c6f6f198d78fb93b74713a69088148
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-04-22 23:44:46 +0800

    update

---
+++ src/num2452.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class num2452 {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> ans = new ArrayList<>();
+        for (String query : queries) {
+            for (String s : dictionary) {
+                int dis = 0;
+                for (int i = 0; i < query.length(); i++) {
+                    if (query.charAt(i) != s.charAt(i)) {
+                        dis++;
+                    }
+                }
+                if (dis <= 2) {
+                    ans.add(query);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
