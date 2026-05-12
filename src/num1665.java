71cb8fa20bf2c1383c78e567464509e673d64ce6
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-05-12 21:54:55 +0800

    update

---
+++ src/num1665.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class num1665 {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> a[1] - a[0] - (b[1] - b[0]));
+        int ans = 0;
+        for (int[] task : tasks) {
+            ans = Math.max(ans + task[0], task[1]);
+        }
+        return ans;
+    }
+}
