4e0d87c3552a1fa89555e38579f6342b8cff42a7
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-05-22 23:03:24 +0800

    update

--- src/num33.java
+++ src/num33.java
@@ -22,24 +22,46 @@ public class num33 {
 //        }
 //        return -1;
 //    }
+//    public int search(int[] nums, int target) {
+//        int n = nums.length;
+//        if(n==1)return nums[0]==target?0:-1;
+//        int l=0,r=n-1;
+//        while (l<=r){
+//            int mid=(l+r)/2;
+//            if(nums[mid]==target)return mid;
+//            if(nums[0]<=nums[mid]){
+//                if(nums[0]<=target&&target<=nums[mid]){
+//                    r=mid-1;
+//                }else {
+//                    l=mid+1;
+//                }
+//            }else {
+//                if(nums[mid]<=target&&target<=nums[n-1]){
+//                    l=mid+1;
+//                }else {
+//                    r=mid-1;
+//                }
+//            }
+//        }
+//        return -1;
+//    }
     public int search(int[] nums, int target) {
         int n = nums.length;
-        if(n==1)return nums[0]==target?0:-1;
         int l=0,r=n-1;
-        while (l<=r){
-            int mid=(l+r)/2;
-            if(nums[mid]==target)return mid;
-            if(nums[0]<=nums[mid]){
-                if(nums[0]<=target&&target<=nums[mid]){
-                    r=mid-1;
-                }else {
-                    l=mid+1;
+        while (l<=r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target <= nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
                 }
-            }else {
-                if(nums[mid]<=target&&target<=nums[n-1]){
-                    l=mid+1;
-                }else {
-                    r=mid-1;
+            } else {
+                if (nums[mid] <= target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
                 }
             }
         }
