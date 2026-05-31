adc90482b2e95878d7b23c91dff80d654f77da93
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-05-31 13:41:53 +0800

    update

---
+++ src/num2126.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class num2126 {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long sum = mass;
+        for (int i = 0; i < asteroids.length; i++) {
+            if (sum < asteroids[i]) {
+                return false;
+            }
+            sum += asteroids[i];
+        }
+        return true;
+    }
+}
