24f6825e43e3e49dc52171405ea2a15d9833222b
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-06-26 21:54:29 +0800

    update

---
+++ src/num3739.java
@@ -0,0 +1,22 @@
+public class num3739 {
+    public long countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int[] pre = new int[n * 2 + 1];
+        pre[n] = 1;
+        int cnt = n;
+        long ans = 0, presum = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                presum += pre[cnt];
+                ++cnt;
+                ++pre[cnt];
+            } else {
+                --cnt;
+                presum -= pre[cnt];
+                ++pre[cnt];
+            }
+            ans += presum;
+        }
+        return ans;
+    }
+}
