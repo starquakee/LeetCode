400e7df89500bfe1bdca0ac697057c9157fcb937
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-07-16 23:40:19 +0800

    update

---
+++ src/num3867.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class num3867 {
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+
+        int[] mx = new int[n];
+        int prefixMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; ++i) {
+            prefixMax = Math.max(prefixMax, nums[i]);
+            mx[i] = prefixMax;
+        }
+
+        int[] prefixGcd = new int[n];
+        for (int i = 0; i < n; ++i) {
+            prefixGcd[i] = gcd(nums[i], mx[i]);
+        }
+
+        Arrays.sort(prefixGcd);
+
+        long ans = 0;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            ans += gcd(prefixGcd[left], prefixGcd[right]);
+            ++left;
+            --right;
+        }
+
+        return ans;
+    }
+
+    public int gcd(int num1, int num2) {
+        if(num2 == 0) return num1;
+        return gcd(num2, num1 % num2);
+    }
+}
