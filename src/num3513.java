2373df73fce01fd686408a9aae6536b995320ac2
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-07-24 21:16:01 +0800

    update

--- src/num3513.java
+++ src/num3513.java
@@ -1,10 +1,34 @@
 public class num3513 {
     public int uniqueXorTriplets(int[] nums) {
-        int n=nums.length;
-        if(n<=2) return n;
-        int ans=1;
-        while (ans<=n) {
-            ans*=2;
+        int n = nums.length;
+        int m = 0;
+        for (int v : nums) {
+            m = Math.max(m, v);
+        }
+        int u = 1;
+        while (u <= m) {
+            u <<= 1;
+        }
+        boolean[] s = new boolean[u];
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                s[nums[i] ^ nums[j]] = true;
+            }
+        }
+        boolean[] t = new boolean[u];
+        for (int x = 0; x < u; x++) {
+            if (!s[x]) {
+                continue;
+            }
+            for (int v : nums) {
+                t[x ^ v] = true;
+            }
+        }
+        int ans = 0;
+        for (boolean b : t) {
+            if (b) {
+                ans++;
+            }
         }
         return ans;
     }
