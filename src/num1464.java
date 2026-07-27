c1381da882f7890f3b28b154292efd76f0d07e23
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-07-27 22:41:19 +0800

    update

---
+++ src/num1464.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class num1464 {
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
+    }
+}
