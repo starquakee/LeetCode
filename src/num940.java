fd0724598c570261540ca7a0ecd21e42a9b5af46
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2026-09-07 22:12:54 +0800

    update

---
+++ src/num940.java
@@ -0,0 +1,20 @@
+public class num940 {
+    public int distinctSubseqII(String s) {
+        final int MOD = 1000000007;
+        int[] g = new int[26];
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int total = 1;
+            for (int j = 0; j < 26; ++j) {
+                total = (total + g[j]) % MOD;
+            }
+            g[s.charAt(i) - 'a'] = total;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            ans = (ans + g[i]) % MOD;
+        }
+        return ans;
+    }
+}
